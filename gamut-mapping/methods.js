12917cfd62d406bb4b782f37fc077ce4ab85f1a3
Lea Verou <lea@verou.me>
2026-06-18 21:16:53 +0300

    [gma][scale] Clamp chroma to P3 max before doing anything

--- gamut-mapping/methods.js
+++ gamut-mapping/methods.js
@@ -49,6 +49,9 @@ const methods = {
 		label: "Scale",
 		description: "Using a midpoint of 0.5, scale the color to fit within the linear P3 gamut.",
 		compute: (color) => {
+			// First, cap chroma to the known P3 maximum (0.37)
+			color = color.to("oklch").set({ c: c => Math.min(c, 0.37) });
+
 			// Make in gamut range symmetrical around 0 [-0.5, 0.5] instead of [0, 1]
 			let deltas = color.to("p3-linear").coords.map(c => c - .5);
 
